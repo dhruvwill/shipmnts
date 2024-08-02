bd2f18ede525169489953fe2a5a351935bb69155
Dhruvwill <ddyhs25@gmail.com>
2024-08-02 17:37:45 +0530

    hotfix: solved api error

--- src/app/api/files/route.ts
+++ src/app/api/files/route.ts
@@ -1,9 +1,9 @@
 import { NextRequest, NextResponse } from "next/server";
-import prismadb from "@/prisma";
+// import prismadb from "@/prisma";
 export async function GET(request: NextRequest, response: NextResponse) {
-  const files = await prismadb.element.findMany();
+  //   const files = await prismadb.element.findMany();
   return Response.json({
     success: true,
-    data: files,
+    // data: files,
   });
 }
